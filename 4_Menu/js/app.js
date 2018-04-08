6fc7ef54dd158ae1968f5c87f59cb669dce7542d
LeszekSadkowski <leszek.sadkowski@gmail.com>
2018-04-08 16:32:30 +0200

    menu

--- 4_Menu/js/app.js
+++ 4_Menu/js/app.js
@@ -1,3 +1,23 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function () {
+
+    var lists = document.querySelectorAll('.nav>ul>li');
+
+    console.log(lists);
+    for (var i = 0; i < lists.length; i++) {
+        lists[i].addEventListener('mouseover', function (ev) {
+
+            var sublist=this.firstElementChild;
+            if(sublist!==null){
+               sublist.style.display='block';
+            }
+
+        });
+        lists[i].addEventListener('mouseout', function (ev) {
+            console.log('fff');
+            var sublist=this.firstElementChild;
+            if(sublist!==null){
+                sublist.style.display='none';
+            }
+        })
+    }
+});
