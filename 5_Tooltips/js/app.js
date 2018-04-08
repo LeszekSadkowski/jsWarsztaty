604d553b2d6669c4fa972a2675654221475a08bb
LeszekSadkowski <leszek.sadkowski@gmail.com>
2018-04-08 17:32:28 +0200

    tooltip

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -1,7 +1,30 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+document.addEventListener('DOMContentLoaded',function(){
 
+    var ttList=document.querySelectorAll('.tooltip');
+    console.log(ttList);
+
+    for( var i=0; i<ttList.length;i++){
+        ttList[i].addEventListener('mouseover',function (evt) {
+            var info=this.dataset.text;
+           var create=document.createElement('span');
+           create.classList.add("tooltipText");
+           create.innerText=info;
+           this.appendChild(create);
+
+
+        });
+
+        ttList[i].addEventListener('mouseout',function (evt) {
+            var ttip=document.querySelector('.tooltipText');
+            ttip.parentElement.removeChild(ttip);
+        });
+    }
+
+
+});
 
 /*
  <span class="tooltipText">Text tooltipa</span>
